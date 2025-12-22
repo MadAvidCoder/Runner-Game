74353c56cb8b76e7cfba7b4d6fa87d870605713d
David Ma <183250896+MadAvidCoder@users.noreply.github.com>
2025-12-23 09:30:46 +1100

    Add Local Variables
    Added all main local variables and states for game logic, to main.lua

---
+++ main.lua
@@ -0,0 +1,21 @@
+local player = {
+    x = 80,
+    y = 0,
+    width = 1,
+    height = 1,
+    velocityY = 0,
+    isJumping = false,
+}
+local ground = { y = 0, height = 40 }
+local obstacles = {}
+local spawnTimer = 0
+local spawnInterval = 1.5
+local gameSpeed = 300
+local score = 0
+local highScore = 0
+local gameOver = false
+local gravity = 1200
+local jumpForce = -500
+
+local images = {}
+local dinoScale = 1
